fe92ae04f1caf2025e83b3267b6bca648af41631
Nathan Moore <ntmoore@gmail.com>
2020-08-24 22:51:43 -0500

    Update 01_blink_LEDs.py

--- 01_blink_LEDs.py
+++ 01_blink_LEDs.py
@@ -18,6 +18,7 @@ while True:
 # Tasks:
 # - Try an on-off cycle of 2:1, 0.2:0.1, 0.002:0.001 etc
 # what is the shortest on-off cycle you can still see (seizure risk)
+# if you want the LED to be at solid 20% brightness, what would the code look like? 
 # Code SOS in Morse
 
 # loops? Blink out an SOS pattern with a foreach loop that specifies duration
