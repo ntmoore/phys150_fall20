b3ebf823b6ca8ca1c253f98072be11d0133ec0ca
Nathan Moore <ntmoore@gmail.com>
2020-08-24 22:50:33 -0500

    Create 01a_foreach_blinking.py

---
+++ 01a_foreach_blinking.py
@@ -0,0 +1,21 @@
+# from adafruit
+# https://learn.adafruit.com/welcome-to-circuitpython/creating-and-editing-code
+#
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+  # note the new line: "a" is a variable that takes the values that are 
+  # listed in square brackets.  This loop is sometimes called a "foreach"
+  for a in [1,2,3,4,5]:
+        print("Hello, CircuitPython!")
+        led.value = True
+        time.sleep(a/10)
+        led.value = False
+        time.sleep(a/10)
+    
+# loops? Blink out an SOS pattern with a foreach loop that specifies duration
