92834e57cf9db95d8e62248e3677ceca468c7de6
Nathan Moore <ntmoore@gmail.com>
2020-09-02 22:22:40 -0500

    Update 01f_sawtooth.py
    This is how I implemented the function.

--- 01f_sawtooth.py
+++ 01f_sawtooth.py
@@ -10,30 +10,45 @@ import time
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 
-# how long dows the rise and fall of the sawtooth last?
-period = 0.5
+# how long does the rise and fall of the sawtooth last?
+period = 0.5 # seconds
 # How many levels do you want?
 num_levels = 10
-#levels go from 0 to num_levels
-levels = range(0,num_levels,1)
-print("levels are: ",levels)
-levels = levels/num_levels
-print("levels are: ",levels)
+# note, if period/num_levels is more than about 2.0s/40 = 50ms, the LED's flashes start to become visible.
+# also, since the pattern is a sawtooth, there are actually 2*num_levels written out during one period
+# thus, warning message
+if((period/num_levels)>(2./40.)):
+    print("use more levels, LED might have visible flashing")
 
 
+# loop over the set of brightness levels that will be written out
+# build a list with .append() function/method
+i=0
+levels=[]
+while (i<num_levels):
+    levels.append(1.0*i/num_levels)
+    i = i+1
+    print("levels are: ",levels)
+while (i>0):
+    levels.append(1.0*i/num_levels)
+    i = i-1
+    print("levels are: ",levels)
+
+#print("length of levels",len(levels))
+#print("period = ",period)
+dt = period/len(levels)
+#print("dt = ",dt)
 
 
+# this implements the periodic sawtooth wave
 while True:
     for brightness in levels:
         # brightness is measured out of a max of 1.0
-        T_fast = 0.01
+        T_fast = dt
         T_on = brightness*T_fast
         T_off = (1-brightness)*T_fast
-        num_repeats=100
-        i=0
-        while (i<num_repeats):
-            led.value = True
-            time.sleep(T_on)
-            led.value = False
-            time.sleep(T_off)
-            i=i+1
+        led.value = True
+        time.sleep(T_on)
+        led.value = False
+        time.sleep(T_off)
+ 
