b04d26a60b3891895d06298cf300ea5501bc094d
AstroDocCarl <cferkinhoff@winona.edu>
2020-09-03 16:18:57 -0500

    Create 03b_snoring_neopixels_carl.py
    An example code that makes ALL of the neo-pixels snore at the same time and color

---
+++ 03b_snoring_neopixels_carl.py
@@ -0,0 +1,26 @@
+# Carl Ferkinhoff
+# starting from from adafruit example
+# https://learn.adafruit.com/welcome-to-circuitpython/creating-and-editing-code
+#
+import time
+from adafruit_circuitplayground import cp
+
+# set up the (red) LED
+#cp.pixels.brightness = 0.1
+#cp.pixels.fill((50,50,50))
+ 
+
+while True:
+    length = 255 #sets the length of the snore
+    j=0
+    wait_down = .001
+    wait_up = .001
+    while (j<length):
+        cp.pixels.fill((255-j, 0, 0))
+        j = j+1
+        time.sleep(wait_down)
+    time.sleep(1)
+    while (j>0):
+        cp.pixels.fill((255-j, 0, 0))
+        j = j-1
+        time.sleep(wait_up)
