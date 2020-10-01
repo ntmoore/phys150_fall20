9e998fb1554b8cfd6f2027a5afc161c8ccbcf762
AstroDocCarl <cferkinhoff@winona.edu>
2020-10-01 14:50:08 -0500

    Update 10_1_log_acceleration_CDF.py
    quickly report the accelerations when not logging

--- 10_1_log_acceleration_CDF.py
+++ 10_1_log_acceleration_CDF.py
@@ -12,13 +12,13 @@ def get_accel_on_button():
     g=9.801
     if cpx.button_a:
         cpx.red_led = True
-        time.sleep(0.05)
+        time.sleep(0.02)
         fp.write('{0:f},{1:f},{2:f},{3:f}\n'.format(current_time,x/g,y/g,z/g))
         print('logging',(current_time,x/g,y/g,z/g))
         fp.flush()
     else:
         cpx.red_led = False
-        time.sleep(0.05)
+        time.sleep(0.1)
         print((current_time,x/g,y/g,z/g))
     return
 
@@ -31,10 +31,18 @@ try:
 
 # will flash the D13 red LED to let us know we can edit the code.py file (i.e. the storage is writeable)
 except OSError as e:
-    delay = 0.5
+    delay = 8
     if e.args[0] == 28:
-        delay = 0.25
+        delay = 4
     while True:
-        cpx.red_led = not cpx.red_led
-        time.sleep(delay)
-        get_accel_on_button()
+        i = 0
+        
+        while (i<delay):
+            get_accel_on_button()
+            cpx.red_led = True
+            i=i+1
+        
+        while (i>0):
+            get_accel_on_button()
+            cpx.red_led = False
+            i=i-1
